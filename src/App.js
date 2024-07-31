8aeaa393e1ca42a22601be09a17adc8c70752f99
Hamza Baig <mail.hamzabaig@gmail.com>
2024-07-31 21:46:04 +0500

    Set up Random Color component initial structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import RandomColor from './components/RandomColor';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RandomColor />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/RandomColor.js
@@ -0,0 +1,4 @@
+const RandomColor = () => {
+  return <div>RandomColor</div>;
+};
+export default RandomColor;
