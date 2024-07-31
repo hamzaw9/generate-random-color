b4e358e3459c7a06c37e7a502750d731d7d0ea48
Hamza Baig <mail.hamzabaig@gmail.com>
2024-07-31 22:06:29 +0500

    Implement random RGB color generation functionality

--- src/components/RandomColor.js
+++ src/components/RandomColor.js
@@ -9,19 +9,26 @@ const RandomColor = () => {
   };
 
   const handleCreateRandomColor = () => {
-    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-    let hexColor = '#';
+    if (typeOfColor == 'hex') {
+      const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+      let hexColor = '#';
 
-    for (let i = 0; i < 6; i++) {
-      hexColor += hex[randomColorUtility(hex.length)];
+      for (let i = 0; i < 6; i++) {
+        hexColor += hex[randomColorUtility(hex.length)];
+      }
+      setColor(hexColor);
+    } else {
+      const r = randomColorUtility(256);
+      const g = randomColorUtility(256);
+      const b = randomColorUtility(256);
+      setColor(`rgb(${r},${g},${b})`);
     }
-    setColor(hexColor);
   };
 
   return (
     <div style={{ width: '100vw', height: '100vh', background: color }}>
-      <button>Create Hex Color</button>
-      <button>Create RGB Color</button>
+      <button onClick={() => setTypeOfColor('hex')}>Create Hex Color</button>
+      <button onClick={() => setTypeOfColor('rgb')}>Create RGB Color</button>
       <button onClick={handleCreateRandomColor}>Generate Random Color</button>
     </div>
   );
