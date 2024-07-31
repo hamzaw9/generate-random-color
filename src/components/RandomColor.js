383d142b7770f01fc850702d9d0715f2eac287ae
Hamza Baig <mail.hamzabaig@gmail.com>
2024-07-31 22:02:39 +0500

    Implement random hex color generation functionality

--- src/components/RandomColor.js
+++ src/components/RandomColor.js
@@ -4,11 +4,25 @@ const RandomColor = () => {
   const [typeOfColor, setTypeOfColor] = useState('hex');
   const [color, setColor] = useState('#000');
 
+  const randomColorUtility = (length) => {
+    return Math.floor(Math.random() * length);
+  };
+
+  const handleCreateRandomColor = () => {
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+    let hexColor = '#';
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomColorUtility(hex.length)];
+    }
+    setColor(hexColor);
+  };
+
   return (
     <div style={{ width: '100vw', height: '100vh', background: color }}>
       <button>Create Hex Color</button>
       <button>Create RGB Color</button>
-      <button>Generate Random Color</button>
+      <button onClick={handleCreateRandomColor}>Generate Random Color</button>
     </div>
   );
 };
