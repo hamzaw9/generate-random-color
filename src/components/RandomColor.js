1c275fb3e8b2f7ab544acbec66cef5dbf9ad1bda
Hamza Baig <mail.hamzabaig@gmail.com>
2024-07-31 21:52:41 +0500

    Add initial JSX structure and state to RandomColor component

--- src/components/RandomColor.js
+++ src/components/RandomColor.js
@@ -1,4 +1,15 @@
+import { useState } from 'react';
+
 const RandomColor = () => {
-  return <div>RandomColor</div>;
+  const [typeOfColor, setTypeOfColor] = useState('hex');
+  const [color, setColor] = useState('#000');
+
+  return (
+    <div style={{ width: '100vw', height: '100vh', background: color }}>
+      <button>Create Hex Color</button>
+      <button>Create RGB Color</button>
+      <button>Generate Random Color</button>
+    </div>
+  );
 };
 export default RandomColor;
